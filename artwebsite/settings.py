60e399271eff3450ae629902fc2469cfe4e97588
Tharuka Devendra <tsdevendra1@gmail.com>
2019-07-28 18:29:39 +0100

    git ignore change and postgres integration

--- artwebsite/settings.py
+++ artwebsite/settings.py
@@ -78,11 +78,18 @@ WSGI_APPLICATION = 'artwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASE_NAME = 'artwebsite_database'
+DATABASE_USER = 'tharukadevendra'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
+
 }
 
 # Password validation
--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import {render} from 'react-dom';
 import App from './App';
 
-alert('hi');
-
 render(
     <App />,
     document.getElementById('root')
